f991eb308c21d90b993560c53bc0c15574ef0a23
thulfkcar <thulfkcar45@gmail.com>
2022-08-22 12:03:41 +0300

    profiling child....

--- kid_garden_app/lib/presentation/ui/Child/childProfileScreen/ChildProfileScreen.dart
+++ kid_garden_app/lib/presentation/ui/Child/childProfileScreen/ChildProfileScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kid_garden_app/presentation/ui/Child/childProfileScreen/ChildProfileViewModel.dart';
 import 'package:kid_garden_app/presentation/utile/language_constrants.dart';
+import 'package:kid_garden_app/them/DentalThem.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import '../../../../data/network/BaseApiService.dart';
 import '../../../../di/Modules.dart';
 import '../../../../domain/AssignRequest.dart';
@@ -48,33 +50,115 @@ class _ChildProfileScreenState extends ConsumerState<ChildProfileScreen> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              IntrinsicHeight(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: childCard(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ClipOval(
+                          child: SizedBox.fromSize(
+                              size: const Size.fromRadius(30),
+                              child: Image.network(
+                                  '$domain${widget.child.image!}',
+                                  fit: BoxFit.cover)),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                10, 0, 0, 0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.child.name,
+                                  style: const TextStyle(
+                                    fontFamily: 'Lexend Deca',
+                                    color: Color(0xFF090F13),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    widget.child.gender == Gender.Female
+                                        ? Icon(
+                                            Icons.female,
+                                            // if child is female gender
+                                            // Icons.male,
+                                            color: ColorStyle.female1,
+                                            // color: ColorStyle.male1,
+                                            size: 20,
+                                          )
+                                        : Icon(
+                                            Icons.male,
+                                            // if child is female gender
+                                            // Icons.male,
+                                            color: ColorStyle.male1,
+                                            // color: ColorStyle.male1,
+                                            size: 20,
+                                          ),
+                                    Text(
+                                      widget.child.age.toString(),
+                                      style: const TextStyle(
+                                        fontFamily: 'Lexend Deca',
+                                        color: Color(0xFF57636C),
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                widget.child.staffName != null
+                                    ? Text(
+                                        "Taking care by: " +
+                                            widget.child.staffName.toString(),
+                                        style: const TextStyle(
+                                          fontFamily: 'Lexend Deca',
+                                          color: Color(0xFF57636C),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      )
+                                    : Container(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      color: ColorStyle.text1,
-                      width: 1,
-                    ),
-                    actionButton()
-                  ],
-                ),
+                  ),
+                  contactCard(widget.child.contacts),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (!widget.isSubscriptionValid) {
+                        Navigator.pop(context);
+                      } else {
+                        await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ChildActions(
+                                      childId: widget.child.id,
+                                    )));
+                      }
+                    },
+                    child: titleText(
+                        getTranslated("actions", context), Colors.black),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(KidThem.white),
+                        side: MaterialStateProperty.all(
+                            BorderSide(width: 1, color: KidThem.male1)),
+                        elevation: MaterialStateProperty.all(0)),
+                  )
+                ],
               ),
-              Container(
-                color: ColorStyle.text1,
-                height: 1,
-              ),
-              contactCard(widget.child.contacts),
-              (widget.child.contacts != null &&
-                      widget.child.contacts!.isNotEmpty)
-                  ? Container(
-                      color: ColorStyle.text1,
-                      height: 1,
-                    )
-                  : Container(),
               widget.child.assignRequest != null
                   ? widget.child.assignRequest!.requestStatus ==
                           RequestStatus.Pending
@@ -88,6 +172,21 @@ class _ChildProfileScreenState extends ConsumerState<ChildProfileScreen> {
                           id: widget.child.assignRequest!.kindergartenId,
                         ))
                   : joinKindergartenCard(),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      titleText(getTranslated("childId", context), Colors.black),
+                      QrImage(
+                        data: widget.child.id,
+                        version: QrVersions.auto,
+                        size: 300,
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ));
@@ -111,7 +210,7 @@ class _ChildProfileScreenState extends ConsumerState<ChildProfileScreen> {
                 );
               },
               separatorBuilder: (BuildContext context, int index) {
-                return Container(height: 1, color: Colors.black);
+                return Container(height: 1, color: Colors.transparent);
               },
             ),
           )
@@ -147,148 +246,12 @@ class _ChildProfileScreenState extends ConsumerState<ChildProfileScreen> {
           )
         ],
       ),
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(ColorStyle.text4),
-          elevation: MaterialStateProperty.all(0),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(10),
-                bottomLeft: Radius.circular(10)),
-          ))),
-    );
-  }
-
-  Widget childCard() {
-    return ElevatedButton(
-      onPressed: () {
-        if (!widget.isSubscriptionValid) Navigator.pop(context);
-      },
-      child: Padding(
-        padding: const EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ClipOval(
-              child: SizedBox.fromSize(
-                  size: const Size.fromRadius(30),
-                  child: Image.network('$domain${widget.child.image!}',
-                      fit: BoxFit.cover)),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.child.name,
-                      style: const TextStyle(
-                        fontFamily: 'Lexend Deca',
-                        color: Color(0xFF090F13),
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        widget.child.gender == Gender.Female
-                            ? Icon(
-                                Icons.female,
-                                // if child is female gender
-                                // Icons.male,
-                                color: ColorStyle.female1,
-                                // color: ColorStyle.male1,
-                                size: 20,
-                              )
-                            : Icon(
-                                Icons.male,
-                                // if child is female gender
-                                // Icons.male,
-                                color: ColorStyle.male1,
-                                // color: ColorStyle.male1,
-                                size: 20,
-                              ),
-                        Text(
-                          widget.child.age.toString(),
-                          style: const TextStyle(
-                            fontFamily: 'Lexend Deca',
-                            color: Color(0xFF57636C),
-                            fontSize: 18,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                      ],
-                    ),
-                    widget.child.staffName != null
-                        ? Text(
-                            "Taking care by: " +
-                                widget.child.staffName.toString(),
-                            style: const TextStyle(
-                              fontFamily: 'Lexend Deca',
-                              color: Color(0xFF57636C),
-                              fontSize: 18,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          )
-                        : Container(),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(ColorStyle.text4),
-        elevation: MaterialStateProperty.all(0),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          Directionality.of(context).index == 0
-              ? const RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.only(topRight: Radius.circular(10)),
-                )
-              : const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
-                ),
-        ),
-      ),
-    );
-  }
-
-  Widget actionButton() {
-    return ElevatedButton(
-      onPressed: () async {
-        if (!widget.isSubscriptionValid) {
-          Navigator.pop(context);
-        } else {
-          await Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => ChildActions(
-                        childId: widget.child.id,
-                      )));
-        }
-      },
-      child: titleText(getTranslated("actions", context), ColorStyle.white),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(ColorStyle.text3),
-        elevation: MaterialStateProperty.all(0),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          Directionality.of(context).index == 0
-              ? const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
-                )
-              : const RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.only(topRight: Radius.circular(10)),
-                ),
-        ),
-      ),
+      style:ButtonStyle(
+          backgroundColor:
+          MaterialStateProperty.all(KidThem.white),
+          side: MaterialStateProperty.all(
+              BorderSide(width: 1, color: KidThem.male1)),
+          elevation: MaterialStateProperty.all(0)),
     );
   }
 }
